5cb2c2f0ffbc8184fd06d7f299646a22f1e7089f
zmdld11 <min1286345855@gmail.com>
2026-09-19 13:11:57 +0800

    chore(release): bump v0.5.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "io.github.zmdld11.shuschedule"
         minSdk = 26
         targetSdk = 35
-        versionCode = 13
-        versionName = "0.5.2"
+        versionCode = 14
+        versionName = "0.5.3"
         vectorDrawables { useSupportLibrary = true }
     }
 
